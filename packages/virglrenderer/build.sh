5adc44a06b95ce892862e65a60b05c530969a1cf
Anonymus04 <joaobat0102@gmail.com>
2025-05-10 18:26:40 -0300

    Update build.sh

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -1,3 +1,3 @@
 SRC_URL=https://github.com/jhinzuo/virglrenderer-venus/archive/refs/tags/1.0.1.tar.gz
-MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCHITECTURE --libdir lib -Dplatforms=egl,glx -Dvenus=true"
+MESON_ARGS="--cross-file=../../../meson-cross-file-$ARCH --libdir lib -Dplatforms=egl,glx -Dvenus=true"
 CFLAGS="-I$PREFIX/include"
