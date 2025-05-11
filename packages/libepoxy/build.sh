d43ad5fefc102870c51a6bba7daba1aa6b34b1b8
Anonymus04 <joaobat0102@gmail.com>
2025-05-11 09:18:50 -0300

    Update build.sh

--- packages/libepoxy/build.sh
+++ packages/libepoxy/build.sh
@@ -1,2 +1,2 @@
 SRC_URL=https://github.com/anholt/libepoxy/archive/refs/tags/1.5.10.tar.gz
-MESON_ARGS="-Degl=yes -Dglx=yes -Dx11=false -Ddefault_library=static"
+MESON_ARGS="-Degl=yes -Dglx=yes -Dx11=true -Ddefault_library=static"
