d21431589f549349c71eb40b0867cdcc2c578e82
Anonymus04 <joaobat0102@gmail.com>
2025-05-11 09:34:21 -0300

    Update build.sh

--- packages/virglrenderer/build.sh
+++ packages/virglrenderer/build.sh
@@ -1,3 +1,3 @@
 SRC_URL=https://github.com/jhinzuo/virglrenderer-venus/archive/refs/tags/1.0.1.tar.gz
 MESON_ARGS="-Dplatforms=egl,glx -Dvenus=true"
-CFLAGS="-I$PREFIX/include"
+CFLAGS="-Wno-gnu-offsetof-extensions -I$PREFIX/include"
