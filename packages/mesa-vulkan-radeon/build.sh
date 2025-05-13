3d0ae8d254adcae04b8c2cc5f8cbeebf05aac930
jhinzuo <68930317+jhinzuo@users.noreply.github.com>
2025-05-13 20:34:05 +0200

    Update build.sh

--- packages/mesa-vulkan-radeon/build.sh
+++ packages/mesa-vulkan-radeon/build.sh
@@ -3,9 +3,9 @@ PKG_CATEGORY="VulkanDriver"
 PKG_PRETTY_NAME="Mesa Radeon Driver"
 VK_DRIVER_LIB="libvulkan_radeon.so"
 
-BLACKLIST_ARCH=aarch64
+#BLACKLIST_ARCH=aarch64
 
 SRC_URL=https://archive.mesa3d.org/mesa-$PKG_VER.tar.xz
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
-CPPFLAGS="-D__USE_GNU"
+#CPPFLAGS="-D__USE_GNU"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=amd -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
