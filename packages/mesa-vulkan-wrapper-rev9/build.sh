9a6ee1796e4cc630407a1d2e814e5d7978a8aea3
Toko kudu <joaobat0102@gmail.com>
2025-07-24 12:39:15 -0300

    Update build.sh

--- packages/mesa-vulkan-wrapper-rev9/build.sh
+++ packages/mesa-vulkan-wrapper-rev9/build.sh
@@ -1,9 +1,10 @@
 PKG_VER=24.2.5
 PKG_CATEGORY="VulkanDriver"
-PKG_PRETTY_NAME="Mesa Android Wrapper (Rev 9)"
+PKG_PRETTY_NAME="Mesa Android Wrapper By LeeGao (Rev 9)"
 PKG_OPTIONAL=1
 VK_DRIVER_LIB="libvulkan_wrapper.so"
 
-SRC_URL=https://archive.mesa3d.org/mesa-$PKG_VER.tar.xz
+GIT_URL=https://github.com/leegao/bionic-vulkan-wrapper
+GIT_COMMIT=664e59a3539e62874884d407a3eb6c4d356e99c4
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
