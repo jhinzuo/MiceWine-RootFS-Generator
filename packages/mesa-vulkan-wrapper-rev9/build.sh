b75224a536f0b97de24c15feeecd085e7ac7d1ca
Toko kudu <joaobat0102@gmail.com>
2025-07-25 16:31:31 -0300

    Update build.sh

--- packages/mesa-vulkan-wrapper-rev9/build.sh
+++ packages/mesa-vulkan-wrapper-rev9/build.sh
@@ -6,5 +6,5 @@ VK_DRIVER_LIB="libvulkan_wrapper.so"
 
 GIT_URL=https://github.com/tokokudo/bionic-vulkan-wrapper
 GIT_COMMIT=a06cdae83f1aa17844c4c4c0728327f84566b7be
-LDFLAGS="-L$PREFIX/lib -landroid-shmem"
+LDFLAGS="-L$PREFIX/lib -landroid-shmem -ladrenotools -llinkernsbypass"
 MESON_ARGS="-Dgallium-drivers= -Dvulkan-drivers=wrapper -Dglvnd=disabled -Dplatforms=x11 -Dxmlconfig=enabled -Dllvm=disabled -Dopengl=false -Degl=disabled -Dzstd=enabled"
